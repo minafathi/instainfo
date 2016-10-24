11f0daeeb07cbd44edc7ea78961c3a7e92007eb1
Trangell <shahryar.tbiz@gmail.com>
2016-10-24 18:11:51 +0330

    add copyright - license - params end 1.0.0

--- mod_instainfo.php
+++ mod_instainfo.php
@@ -1,24 +1,12 @@
 <?php
-/**
- * Hello World! Module Entry Point
- * 
- * @package    Joomla.Tutorials
- * @subpackage Modules
- * @license    GNU/GPL, see LICENSE.php
- * @link       http://docs.joomla.org/J3.x:Creating_a_simple_module/Developing_a_Basic_Module
- * mod_helloworld is free software. This version may have been modified pursuant
- * to the GNU General Public License, and as distributed it includes or
- * is derivative of works licensed under the GNU General Public License or
- * other free or open source software licenses.
- */
- 
-// No direct access
 defined('_JEXEC') or die;
+/**
+ * @package     Joomla.Site module
+ * @subpackage  mod_instainfo
+ * @author      trangell team https://trangell.com
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
 // Include the syndicate functions only once
 require_once dirname(__FILE__) . '/helper.php';
-// $hello = modInStAiNfO::getHello($params);
-// $count = count($hello);
-// $post = modInStAiNfO::posts($params);
-// $count = count($post);
 require JModuleHelper::getLayoutPath('mod_instainfo');
 ?>
